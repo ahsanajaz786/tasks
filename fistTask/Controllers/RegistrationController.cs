6cdd84e6ae26eca1912b63910f1ac8d878e840c8
ahsanajaz786 <ahsanajaz2@gmail.com>
2019-08-02 11:40:44 +0500

    tsk1

---
+++ fistTask/Controllers/RegistrationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace fistTask.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        taskEntities db = new taskEntities();
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllData()
+        {
+            return View();
+        }
+        public JsonResult jsonData()
+        {
+            List<datum> d = db.data.ToList();
+            return Json(d, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpPost]
+        public void SaveData(string namen,string lname,string email,string phoneno)
+        {
+            
+            datum d
+                = new datum();
+            d.email = email;
+            d.name = namen;
+            d.lastname = lname;
+            d.phone = phoneno;
+            db.data.Add(d);
+            db.SaveChanges();
+            Response.Redirect("/Registration/AllData");
+
+
+        }
+    }
+}
